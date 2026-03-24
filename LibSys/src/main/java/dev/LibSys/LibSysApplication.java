0f11be4d2a5590dbc99b5757b61294bfa4287436
ricardo <ricardokohlermk@gmail.com>
2026-03-23 21:58:24 -0300

    chore: initial setup

---
+++ LibSys/src/main/java/dev/LibSys/LibSysApplication.java
@@ -0,0 +1,13 @@
+package dev.LibSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibSysApplication.class, args);
+	}
+
+}
---
+++ LibSys/src/test/java/dev/LibSys/LibSysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.LibSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
